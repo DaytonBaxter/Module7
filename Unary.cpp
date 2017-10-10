52979a204fb4b229f95ee4cb5ef205a223989ce6
daytonbaxter <baxterd23@gmail.com>
2017-10-10 12:05:45 -0600

    Initial commit

---
+++ Unary.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+private:
+    unsigned int count;  //count
+
+public:
+    Counter(): count(0) //constructor
+    {};
+
+    unsigned int get_count ()   //returns count
+    {
+        return count;
+    }
+    void operator ++()      //increment prefix
+    {
+        ++count;
+    }
+};
+
+int main()
+{
+    Counter c1, c2;
+
+    cout << "c1 = " << c1.get_count() << endl;
+    cout << "c2 = " << c2.get_count() << endl;
+
+    ++c1;
+    ++c2;
+    ++c2;
+    ++c2;
+    ++c2;
+
+    cout << "c1 = " << c1.get_count() << endl;
+    cout << "c2 = " << c2.get_count() << endl;
+
+
+
+    return 0;
+}
